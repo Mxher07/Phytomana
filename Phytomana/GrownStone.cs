c81deb3a29612d3303ba9a6821c8cd75343df843
Wurozaye <2360881635p@gmail.com>
2026-08-29 08:59:06 +0000

    GrownWood&GrownStone

---
+++ Phytomana/GrownStone.cs
@@ -0,0 +1,22 @@
+﻿using Engine;
+using Engine.Graphics;
+
+namespace Game {
+    public class GorwnStone : CubeBlock {
+        public Texture2D m_texture;
+
+        public override void Initialize() {
+            base.Initialize();
+            CanBeBuiltIntoFurniture = true;
+            m_texture = ContentManager.Get<Texture2D>("Textures/PhytoMana/GrownStone");
+            int contents = BlocksManager.GetBlockIndex<TemplateBlock>();
+            Log.Information($"[PhytoMana]{contents} Registered.");
+        }
+
+        public override int GetFaceTextureSlot(int face, int value) => face == 4 ? 1 : 0;
+
+        public override int GetTextureSlotCount(int value) => 2;
+
+        public override Texture2D GetDefaultTexture(int value) => m_texture;
+    }
+}
