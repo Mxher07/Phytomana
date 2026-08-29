87a856bcede4a0e1a22f1ffe059b20c71f837a1d
Wurozaye <2360881635p@gmail.com>
2026-08-29 17:41:18 +0000

    Particle part1

--- Phytomana/Subsystems/SubsystemGrownDaisyBehavior.cs
+++ Phytomana/Subsystems/SubsystemGrownDaisyBehavior.cs
@@ -2,6 +2,7 @@
 using Engine;
 using GameEntitySystem;
 using TemplatesDatabase;
+using Game;
 
 namespace Game {
     public class SubsystemGrownDaisyBehavior : SubsystemBlockBehavior, IUpdateable {
@@ -11,12 +12,13 @@ namespace Game {
         }
 
         public class ConvertData {
-            public Point3 DaisyPoint;
-            public Point3 Point;
-            public int SourceContents;
-            public int DestinationContents;
-            public double ConvertTime;
-        }
+    public Point3 DaisyPoint;
+    public Point3 Point;
+    public int SourceContents;
+    public int DestinationContents;
+    public double ConvertTime;
+    public bool ParticleSpawned;
+}
 
         public const double ScanInterval = 1.0;
         public const double ConvertDuration = 60.0;
@@ -43,18 +45,22 @@ namespace Game {
         public override int[] HandledBlocks => [BlocksManager.GetBlockIndex<GrownDaisyFlower>()];
 
         public UpdateOrder UpdateOrder => UpdateOrder.Default;
+        
+        public SubsystemTerrain m_subsystemTerrain;
+        public List<ManaParticleSystem> m_activeParticleSystems = [];
 
         public override void Load(ValuesDictionary valuesDictionary) {
-            base.Load(valuesDictionary);
-            m_subsystemGameInfo = Project.FindSubsystem<SubsystemGameInfo>(true);
-            m_graniteBlockIndex = BlocksManager.GetBlockIndex<GraniteBlock>();
-            m_grownWoodBlockIndex = BlocksManager.GetBlockIndex<GrownWoodBlock>();
-            m_grownStoneBlockIndex = BlocksManager.GetBlockIndex<GrownStoneBlock>();
-            foreach (int woodSourceBlockIndex in m_woodSourceBlockIndexes) {
-                m_sourceToDestination[woodSourceBlockIndex] = m_grownWoodBlockIndex;
-            }
-            m_sourceToDestination[m_graniteBlockIndex] = m_grownStoneBlockIndex;
-        }
+    base.Load(valuesDictionary);
+    m_subsystemGameInfo = Project.FindSubsystem<SubsystemGameInfo>(true);
+    m_subsystemTerrain = Project.FindSubsystem<SubsystemTerrain>(true);
+    m_graniteBlockIndex = BlocksManager.GetBlockIndex<GraniteBlock>();
+    m_grownWoodBlockIndex = BlocksManager.GetBlockIndex<GrownWoodBlock>();
+    m_grownStoneBlockIndex = BlocksManager.GetBlockIndex<GrownStoneBlock>();
+    foreach (int woodSourceBlockIndex in m_woodSourceBlockIndexes) {
+        m_sourceToDestination[woodSourceBlockIndex] = m_grownWoodBlockIndex;
+    }
+    m_sourceToDestination[m_graniteBlockIndex] = m_grownStoneBlockIndex;
+}
 
         public override void OnBlockAdded(int value, int oldValue, int x, int y, int z) {
             AddDaisy(x, y, z);
@@ -84,10 +90,33 @@ namespace Game {
         }
 
         public virtual void Update(float dt) {
-            double time = m_subsystemGameInfo.TotalElapsedGameTime;
-            ProcessConversions(time);
-            ScanDaisies(time);
+    double time = m_subsystemGameInfo.TotalElapsedGameTime;
+    
+    UpdateParticleSystems(dt);
+    
+    ProcessConversions(time);
+    ScanDaisies(time);
+    SpawnIdleParticles(time);
+}
+        
+        public void SpawnIdleParticles(double time) {
+    foreach (ConvertData conversion in m_conversions.Values) {
+        if (time >= conversion.ConvertTime - 2f && !conversion.ParticleSpawned) {
+            conversion.ParticleSpawned = true;
+            
+            Vector3 position = new Vector3(conversion.Point.X, conversion.Point.Y, conversion.Point.Z);
+            
+            ManaParticleSystem particleSystem = new ManaParticleSystem(
+                position,    // 位置
+                2f,          // 大小
+                2f,          // 持续时间
+                Color.White  // 白色
+            );
+            
+            m_activeParticleSystems.Add(particleSystem);
         }
+    }
+}
 
         public void ProcessConversions(double time) {
             m_dueConversions.Clear();
@@ -102,18 +131,29 @@ namespace Game {
         }
 
         public void ApplyConversion(ConvertData conversion) {
-            int x = conversion.Point.X;
-            int y = conversion.Point.Y;
-            int z = conversion.Point.Z;
-            TerrainChunk chunk = SubsystemTerrain.Terrain.GetChunkAtCell(x, z);
-            if (chunk == null || chunk.State != TerrainChunkState.Valid) {
-                return;
-            }
-            if (SubsystemTerrain.Terrain.GetCellContents(x, y, z) == conversion.SourceContents) {
-                SubsystemTerrain.ChangeCell(x, y, z, Terrain.MakeBlockValue(conversion.DestinationContents, 0, 0));
-            }
-            m_conversions.Remove(conversion.Point);
-        }
+    int x = conversion.Point.X;
+    int y = conversion.Point.Y;
+    int z = conversion.Point.Z;
+    
+    TerrainChunk chunk = m_subsystemTerrain.Terrain.GetChunkAtCell(x, z);
+    if (chunk == null || chunk.State != TerrainChunkState.Valid) {
+        return;
+    }
+    
+    if (m_subsystemTerrain.Terrain.GetCellContents(x, y, z) == conversion.SourceContents) {
+        m_subsystemTerrain.ChangeCell(x, y, z, Terrain.MakeBlockValue(conversion.DestinationContents, 0, 0));
+        
+        Vector3 position = new Vector3(x, y, z);
+        ManaParticleSystem completionParticle = new ManaParticleSystem(
+            position,   // 位置
+            2f,         // 大小
+            2f,         // 持续时间
+            Color.White // 白色
+        );
+        m_activeParticleSystems.Add(completionParticle);
+    }
+    m_conversions.Remove(conversion.Point);
+}
 
         public void ScanDaisies(double time) {
             foreach (DaisyData daisy in m_daisies.Values) {
@@ -125,44 +165,63 @@ namespace Game {
         }
 
         public void ScanAroundDaisy(DaisyData daisy) {
-            int x = daisy.Point.X;
-            int y = daisy.Point.Y;
-            int z = daisy.Point.Z;
-            TerrainChunk chunk = SubsystemTerrain.Terrain.GetChunkAtCell(x, z);
-            if (chunk == null || chunk.State != TerrainChunkState.Valid) {
-                return;
+    int x = daisy.Point.X;
+    int y = daisy.Point.Y;
+    int z = daisy.Point.Z;
+    
+    TerrainChunk chunk = m_subsystemTerrain.Terrain.GetChunkAtCell(x, z);
+    if (chunk == null || chunk.State != TerrainChunkState.Valid) {
+        return;
+    }
+    
+    for (int i = x - 1; i <= x + 1; i++) {
+        for (int j = z - 1; j <= z + 1; j++) {
+            TerrainChunk cellChunk = m_subsystemTerrain.Terrain.GetChunkAtCell(i, j);
+            if (cellChunk == null || cellChunk.State != TerrainChunkState.Valid) {
+                continue;
             }
-            for (int i = x - 1; i <= x + 1; i++) {
-                for (int j = z - 1; j <= z + 1; j++) {
-                    TerrainChunk cellChunk = SubsystemTerrain.Terrain.GetChunkAtCell(i, j);
-                    if (cellChunk == null || cellChunk.State != TerrainChunkState.Valid) {
-                        continue;
-                    }
-                    Point3 point = new(i, y, j);
-                    int cellContents = SubsystemTerrain.Terrain.GetCellContents(i, y, j);
-                    m_conversions.TryGetValue(point, out ConvertData conversion);
-                    if (m_sourceToDestination.TryGetValue(cellContents, out int destinationContents)) {
-                        if (conversion == null) {
-                            AddConversion(daisy.Point, point, cellContents, destinationContents);
-                        }
-                    }
-                    else if (conversion != null
-                        && conversion.DaisyPoint == daisy.Point) {
-                        m_conversions.Remove(point);
-                    }
+            
+            Point3 point = new(i, y, j);
+            int cellContents = m_subsystemTerrain.Terrain.GetCellContents(i, y, j);
+            m_conversions.TryGetValue(point, out ConvertData conversion);
+            
+            if (m_sourceToDestination.TryGetValue(cellContents, out int destinationContents)) {
+                if (conversion == null) {
+                    AddConversion(daisy.Point, point, cellContents, destinationContents);
                 }
             }
+            else if (conversion != null && conversion.DaisyPoint == daisy.Point) {
+                m_conversions.Remove(point);
+            }
         }
+    }
+}
+
+    public void UpdateParticleSystems(float dt) {
+    List<ManaParticleSystem> finishedSystems = [];
+    foreach (ManaParticleSystem system in m_activeParticleSystems) {
+        bool isFinished = system.Simulate(dt);
+        if (isFinished) {
+            finishedSystems.Add(system);
+        }
+    }
+    
+    foreach (ManaParticleSystem system in finishedSystems) {
+        m_activeParticleSystems.Remove(system);
+    }
+}
+
 
         public void AddConversion(Point3 daisyPoint, Point3 point, int sourceContents, int destinationContents) {
-            m_conversions[point] = new ConvertData {
-                DaisyPoint = daisyPoint,
-                Point = point,
-                SourceContents = sourceContents,
-                DestinationContents = destinationContents,
-                ConvertTime = m_subsystemGameInfo.TotalElapsedGameTime + ConvertDuration
-            };
-        }
+    m_conversions[point] = new ConvertData {
+        DaisyPoint = daisyPoint,
+        Point = point,
+        SourceContents = sourceContents,
+        DestinationContents = destinationContents,
+        ConvertTime = m_subsystemGameInfo.TotalElapsedGameTime + ConvertDuration,
+        ParticleSpawned = false // 新增
+    };
+}
 
         public void AddDaisy(int x, int y, int z) {
             Point3 point = new(x, y, z);
---
+++ Phytomana/Systems/ManaParticleSystem.cs
@@ -0,0 +1,154 @@
+using Engine;
+using Engine.Graphics;
+using System;
+using System.Collections.Generic;
+using Game;
+
+namespace Game {
+    public class ManaParticleSystem : ParticleSystem<ManaParticleSystem.Particle> {
+        public class Particle : Game.Particle {
+            public Vector3 Velocity;
+            public float TimeToLive;
+            public float MaxTimeToLive;
+            public Vector3 StartPosition;
+            public Vector3? TargetPosition;
+            public Color[] Colors;
+            public float CurrentColorIndex;
+            public float StartSize;
+            public bool IsFading;
+        }
+
+        private Random m_random = new Random();
+        private bool m_isMultiColor;
+
+        public ManaParticleSystem(Vector3 position, float size, float duration, Color color, Vector3? targetPosition = null) 
+            : base(32) {
+            InitializeParticles(position, size, duration, new Color[] { color }, targetPosition);
+            m_isMultiColor = false;
+        }
+
+        public ManaParticleSystem(Vector3 position, float size, float duration, Color[] colors, Vector3? targetPosition = null) 
+            : base(32) {
+            if (colors == null || colors.Length < 2) {
+                throw new ArgumentException("Colors are null!");
+            }
+            InitializeParticles(position, size, duration, colors, targetPosition);
+            m_isMultiColor = true;
+        }
+
+        private void InitializeParticles(Vector3 position, float size, float duration, Color[] colors, Vector3? targetPosition) {
+            Texture = ContentManager.Get<Texture2D>("Textures/PhytoMana/Mana");
+            TextureSlotsCount = 1;
+            
+            Color[] colorsCopy = new Color[colors.Length];
+            Array.Copy(colors, colorsCopy, colors.Length);
+
+            for (int i = 0; i < Particles.Length; i++) {
+                Particle particle = Particles[i];
+                particle.IsActive = true;
+                particle.StartPosition = position + 0.4f * size * new Vector3(
+                    m_random.Float(-1f, 1f), 
+                    m_random.Float(-1f, 1f), 
+                    m_random.Float(-1f, 1f)
+                );
+                particle.Position = particle.StartPosition;
+                particle.TargetPosition = targetPosition;
+                particle.TimeToLive = duration;
+                particle.MaxTimeToLive = duration;
+                particle.Colors = colorsCopy;
+                particle.CurrentColorIndex = 0f;
+                particle.StartSize = 0.3f * size;
+                particle.Size = new Vector2(particle.StartSize);
+                particle.IsFading = false;
+                particle.FlipX = m_random.Bool();
+                particle.FlipY = m_random.Bool();
+                
+                particle.Velocity = 0.5f * size * new Vector3(
+                    m_random.Float(-1f, 1f), 
+                    m_random.Float(-1f, 1f), 
+                    m_random.Float(-1f, 1f)
+                );
+            }
+        }
+
+        public override bool Simulate(float dt) {
+            dt = Math.Clamp(dt, 0f, 0.1f);
+            float num = MathF.Pow(0.1f, dt);
+            bool hasActiveParticles = false;
+
+            for (int i = 0; i < Particles.Length; i++) {
+                Particle particle = Particles[i];
+                if (!particle.IsActive) continue;
+
+                hasActiveParticles = true;
+                particle.TimeToLive -= dt;
+
+                if (particle.TimeToLive <= 0f) {
+                    particle.IsActive = false;
+                    continue;
+                }
+
+                float lifeProgress = 1f - (particle.TimeToLive / particle.MaxTimeToLive);
+                float remainingRatio = particle.TimeToLive / particle.MaxTimeToLive;
+
+                if (particle.TargetPosition.HasValue) {
+                    float moveProgress = Math.Min(lifeProgress / 0.7f, 1f);
+                    particle.Position = Vector3.Lerp(particle.StartPosition, particle.TargetPosition.Value, moveProgress);
+                } else {
+                    particle.Position += particle.Velocity * dt;
+                    particle.Velocity *= num;
+                }
+
+                if (particle.Colors != null && particle.Colors.Length > 0) {
+                    if (particle.Colors.Length == 1) {
+                        particle.Color = particle.Colors[0];
+                    } else {
+                        float colorProgress = lifeProgress * (particle.Colors.Length - 1);
+                        int index = (int)Math.Floor(colorProgress);
+                        int nextIndex = Math.Min(index + 1, particle.Colors.Length - 1);
+                        float lerpFactor = colorProgress - index;
+                        
+                        Color currentColor = particle.Colors[index];
+                        Color nextColor = particle.Colors[nextIndex];
+                        
+                        particle.Color = Color.Lerp(currentColor, nextColor, lerpFactor);
+                    }
+                    
+                    particle.Color.A = 255;
+                }
+
+                if (remainingRatio <= 0.3f) {
+                    particle.IsFading = true;
+                    float fadeProgress = 1f - (remainingRatio / 0.3f);
+                    float currentSize = particle.StartSize * (1f - fadeProgress);
+                    particle.Size = new Vector2(Math.Max(currentSize, 0.01f));
+                    
+                    if (remainingRatio <= 0f) {
+                        // 这里会在循环末尾处理
+                    }
+                } else {
+                    particle.Size = new Vector2(particle.StartSize);
+                }
+
+                // 销毁
+                if (particle.IsFading && particle.Size.X <= 0.01f) {
+                    particle.TimeToLive = Math.Min(particle.TimeToLive, -0.25f);
+                }
+
+                if (!particle.TargetPosition.HasValue) {
+                    particle.Velocity.Y += 0.5f * dt;
+                }
+            }
+
+            bool allInactive = true;
+            for (int i = 0; i < Particles.Length; i++) {
+                if (Particles[i].IsActive) {
+                    allInactive = false;
+                    break;
+                }
+            }
+
+            return allInactive;
+        }
+    }
+}
