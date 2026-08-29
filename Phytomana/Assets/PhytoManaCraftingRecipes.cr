e03b4abe3bdaaffd3a830faf7dcb0bfe9ac86bda
Wurozaye <2360881635p@gmail.com>
2026-08-29 12:30:26 +0000

    ManaSpreader Part1

---
+++ Phytomana/Assets/PhytoManaCraftingRecipes.cr
@@ -0,0 +1,7 @@
+<PhytoMana>
+  <Recipe Result="ManaSpreaderBlock" ResultCount="1" RequiredHeatLevel="0" a="grownwood" b="ironingot" c="string" d="planks" Description="[0]">
+    "aaa"
+    "bc"
+    "aaa"
+  </Recipe>
+</PhytoMana>
---
+++ Phytomana/Blocks/ManaSpreaderBlock.cs
@@ -0,0 +1,18 @@
+﻿using Engine;
+using Engine.Graphics;
+
+namespace Game {
+    public class ManaSpreaderBlock : CubeBlock {
+        public Texture2D m_texture;
+        public override void Initialize() {
+            base.Initialize();
+            CanBeBuiltIntoFurniture = true;
+            m_texture = ContentManager.Get<Texture2D>("Textures/PhytoMana/ManaSpreader");
+            int contents = BlocksManager.GetBlockIndex<ManaSpreaderBlock>();
+Log.Information($"[PhytoMana]{contents} Registered.");
+        }
+        public override int GetFaceTextureSlot(int face, int value) => face == 4 ? 1 : 0;
+        public override int GetTextureSlotCount(int value) => 2;
+        public override Texture2D GetDefaultTexture(int value) => m_texture;
+    }
+}
