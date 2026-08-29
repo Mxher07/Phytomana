0c528b87e1bbda763009ac5320e02e7a226ed252
Wurozaye <2360881635p@gmail.com>
2026-08-29 09:37:28 +0000

    daisy

---
+++ Phytomana/Blocks/GrownDaisyFlower.cs
@@ -0,0 +1,18 @@
+using Engine;
+using Engine.Graphics;
+
+namespace Game {
+    public class GrownDaisyFlower : FlowerBlock {
+        public Texture2D m_texture;
+        public override void Initialize() {
+            base.Initialize();
+            CanBeBuiltIntoFurniture = true;
+            m_texture = ContentManager.Get<Texture2D>("Textures/PhytoMana/GrownDaisy");
+            int contents = BlocksManager.GetBlockIndex<GrownDaisyFlower>();
+            Log.Information($"[PhytoMana]{contents} Registered.");
+        }
+        public override int GetFaceTextureSlot(int face, int value) => 0;
+        public override int GetTextureSlotCount(int value) => 1;
+        public override Texture2D GetDefaultTexture(int value) => m_texture;
+    }
+}
--- Phytomana/Blocks/GrownWoodBlock.cs
+++ Phytomana/Blocks/GrownWoodBlock.cs
@@ -2,21 +2,17 @@
 using Engine.Graphics;
 
 namespace Game {
-    public class GrownStoneBlock : CubeBlock {
+    public class GrownWoodBlock : CubeBlock {
         public Texture2D m_texture;
-
         public override void Initialize() {
             base.Initialize();
             CanBeBuiltIntoFurniture = true;
-            m_texture = ContentManager.Get<Texture2D>("Textures/PhytoMana/GrownStone");
-            int contents = BlocksManager.GetBlockIndex<GrownStoneBlock>();
+            m_texture = ContentManager.Get<Texture2D>("Textures/PhytoMana/GrownWood");
+            int contents = BlocksManager.GetBlockIndex<GrownWoodBlock>();
             Log.Information($"[PhytoMana]{contents} Registered.");
         }
-
-        public override int GetFaceTextureSlot(int face, int value) => face == 4 ? 1 : 0;
-
-        public override int GetTextureSlotCount(int value) => 2;
-
+        public override int GetFaceTextureSlot(int face, int value) => 0;
+        public override int GetTextureSlotCount(int value) => 1;
         public override Texture2D GetDefaultTexture(int value) => m_texture;
     }
 }
